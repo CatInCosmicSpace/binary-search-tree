17ba3793051575db2714530764ae501afd929ad4
Alena Egorova <anon5007@gmail.com>
2016-05-07 17:29:59 +0300

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -31,40 +31,40 @@ fstream & operator>>(fstream & file, BinarySearchTree<T> &);
 
 template <typename T>
 struct node {
-	typedef unique_ptr<node<T>> ptrNode;
+    typedef unique_ptr<node<T>> ptrNode;
 
-	T			data;
-	ptrNode		left;
-	ptrNode		right;
-	node(const T & value) : data(value), left(nullptr), right(nullptr) {};
+    T			data;
+    ptrNode		left;
+    ptrNode		right;
+    node(const T & value) : data(value), left(nullptr), right(nullptr) {};
 };
 
 template <typename T>
 class BinarySearchTree {
 private:
-	typedef		unique_ptr<node<T>> ptrNode;
-	ptrNode		root;
-	size_t		count;
-	size_t		existed;
+    typedef		unique_ptr<node<T>> ptrNode;
+    ptrNode		root;
+    size_t		count;
+    size_t		existed;
 
-	auto		print(const unique_ptr<node<T>>&, ostream &) -> bool;
-	auto		remove(const T & key, node<T> * leaf) -> node<T> *;
-	auto		findMin(unique_ptr<node<T>>& leaf) -> node<T> *;
-	auto		search(const T &key, node<T>* leaf) -> node<T> *;
+    auto		print(const unique_ptr<node<T>>&, ostream & os = std::cout, size_t width = 0) -> void;
+    auto		remove(const T&, node<T>*) -> node<T> *;
+    auto		findMin(unique_ptr<node<T>>&) -> node<T> *;
+    auto		search(const T&, node<T>*) -> node<T> *;
 public:
-	BinarySearchTree() : root(nullptr), count(0), existed(0) {};			// TESTED
-	BinarySearchTree(size_t k) : root(nullptr), count(k), existed(0) {};	// TESTED
+    BinarySearchTree() : root(nullptr), count(0), existed(0) {};					// TESTED
+    BinarySearchTree(size_t k) : root(nullptr), count(k), existed(0) {};			// TESTED
 
-	auto		insert(const T &node) -> bool;								// TESTED
-	auto		remove(const T &key) -> node<T> *;
-	auto		search(const T &key) -> node<T> *;							// TESTED
-	auto		getCount() const -> size_t;									// TESTED
-	auto		getNumber() const -> size_t;								// TESTED
+    auto		insert(const T&) -> bool;									// TESTED
+    auto		remove(const T&) -> node<T> *;								// tested?..
+    auto		search(const T&) -> node<T> *;								// TESTED
+    auto		getCount() const -> size_t;									// TESTED
+    auto		getNumber() const -> size_t;									// TESTED
 
-	friend		ostream & operator<< <>(ostream &output, BinarySearchTree &);		// TESTED
-	friend		istream & operator>> <>(istream &input, BinarySearchTree &);		// TESTED
-	friend		fstream & operator<< <>(fstream &file, BinarySearchTree<T> &);		// TESTED
-	friend		fstream & operator>> <>(fstream &file, BinarySearchTree<T> &);		// TESTED
+    friend		ostream & operator<< <>(ostream &output, BinarySearchTree &);		// TESTED
+    friend		istream & operator>> <>(istream &input, BinarySearchTree &);		// TESTED
+    friend		fstream & operator<< <>(fstream &file, BinarySearchTree<T> &);		// TESTED
+    friend		fstream & operator>> <>(fstream &file, BinarySearchTree<T> &);		// TESTED
 };
 
 #include "BinarySearchTree.cpp"
