b56beba617249e3a14f0a3f294c1fd8077590041
Alena Egorova <anon5007@gmail.com>
2016-05-18 09:33:06 +0300

    ...

--- binary-search-tree/BinarySearchTree.cpp
+++ binary-search-tree/BinarySearchTree.cpp
@@ -211,12 +211,12 @@ auto BinarySearchTree<T>::getRoot() -> T {
 }
 
 template<typename T>
-auto BinarySearchTree<T>::begin() {
+auto BinarySearchTree<T>::begin() -> std::_Vector_iterator<std::_Simple_types<T>> {
 	return elements.begin();
 }
 
 template<typename T>
-auto BinarySearchTree<T>::end() {
+auto BinarySearchTree<T>::end() -> std::_Vector_iterator<std::_Simple_types<T>> {
 	elements.clear();
 	elements = this->createVector();
 	return elements.end();
